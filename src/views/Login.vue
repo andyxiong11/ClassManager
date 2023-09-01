2a044d7dae42202c4d470b9b5cbf45e9817bcb01
[andyxiong11] <[xiongyexing1999@163.com]>
2023-09-01 21:47:26 +0800

    实现登录页3个圆圈

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <div class="circle-wrapper circle-1"></div>
+    <div class="circle-wrapper circle-2"></div>
+    <div class="circle-wrapper circle-3"></div>
   </div>
 </template>
 
@@ -15,5 +18,32 @@ export default {
     height: 100%;
     background: url("../assets/bg.png");
     background-size: cover;
+  }
+  .circle-wrapper {
+    left: 50%;
+    top: 50%;
+    /* 水平垂直位移，加上上面实现页面居中显示 */
+    transform: translate(-50%, -50%);
+
+    border:1px solid;
+    /* 圆角 */
+    border-radius: 50%;
+    opacity: 0.2;
+    position: absolute;
+  }
+  .circle-1 {
+    width: 487px;
+    height: 487px;
+    background-color: #0c729c;
+  }
+  .circle-2 {
+    width: 420px;
+    height: 420px;
+    background-color: #008cc0;
+  }
+  .circle-3 {
+    width: 359px;
+    height: 359px;
+    background-color: #41a1cd;
   }
 </style>
