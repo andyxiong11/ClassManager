cb41bd5b18b53fd7231152e8d8ad9d6e06f0cdeb
[andyxiong11] <[xiongyexing1999@163.com]>
2023-09-02 21:05:36 +0800

    添加Index路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 import Login from '../views/Login'
+import Index from '../views/Index'
 
 Vue.use(VueRouter)
 
@@ -16,6 +17,11 @@ const router = new VueRouter({
       path:'/login',
       name:'login',
       component:Login
+    },
+    {
+      path:'/index',
+      name:'index',
+      component:Index
     }
   ]
 })
