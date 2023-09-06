8213be26c391cc23594d9645a5cba865c33a6e3c
[andyxiong11] <[xiongyexing1999@163.com]>
2023-09-06 22:46:45 +0800

    新增所有路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,14 @@ import Login from '../views/Login'
 import Index from '../views/Index'
 import Home from '../views/Home.vue'
 
+import ClassStatistic from '../views/class_statistic/ClassStatistic.vue'
+import ClassSchedule from '../views/class_schedule/ClassSchedule.vue'
+import AllDocument from '../views/document_center/AllDocumnet.vue'
+import OwnDocument from '../views/document_center/OwnDocument.vue'
+import LiveClassList from '../views/class_monitor/LiveClassList.vue'
+import LiveClassDetail from '../views/class_monitor/LiveClassDetail.vue'
+import VideoList from '../views/video_class/VideoList.vue'
+
 Vue.use(VueRouter)
 
 //创建路由对象
@@ -27,7 +35,29 @@ const router = new VueRouter({
     {
       path:'/home',
       name:'home',
-      component:Home
+      component:Home,
+      children: [{
+        path: 'class-statistic',
+        component: ClassStatistic
+      }, {
+        path: 'class-schedule',
+        component: ClassSchedule
+      }, {
+        path: 'all-document',
+        component: AllDocument,
+      }, {
+        path: 'own-document',
+        component: OwnDocument,
+      }, {
+        path: 'live-classes',
+        component: LiveClassList,
+      }, {
+        path: 'live-classes-detail',
+        component: LiveClassDetail,
+      }, {
+        path: 'video-list',
+        component: VideoList,
+      }]
     }
   ]
 })
